075272fd2d0d71844372494f5a4d41c736c46839
robincoello <robincoello@hotmail.com>
2017-02-08 21:44:24 +0100

    fix includes extenciones/funciones

--- codigo_fuente/admin/funciones.php
+++ codigo_fuente/admin/funciones.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 function _campo($tabla, $id, $campo) {
     global $conexion;
@@ -8,77 +8,105 @@ function _campo($tabla, $id, $campo) {
     return $reg[$campo];
 }
 
+function _incluir_funciones() {
+       
+    $ruta = "../extenciones/funciones";
 
-
-function _incluir_funciones(){
+    $directorio = opendir($ruta); //ruta actual
+        
+    while ($archivo = readdir($directorio)) { //obtenemos un archivo y luego otro sucesivamente
+        if (is_file($archivo)) {//verificamos si es o no un directorio
+            //echo "[" . $archivo . "]<br />"; //de ser un directorio lo envolvemos entre corchetes    
+            include "$ruta/$archivo";
+         
+        } 
+    }
     
-    $funciones = _listar_directorios_ruta();
-    foreach ($funciones as $valor) {
-        
-        $f = "../extenciones/funciones/$valor.php"; 
-        
-        if(file_exists($f)){
-            include "../extenciones/funciones/$valor.php"; 
+    
+  
+/*
+    $ruta = "../extenciones/funciones";
+
+    $funciones = _listar_directorios_ruta($ruta);
+    foreach ($funciones as $fichero) {
+
+        $f = "$ruta/$fichero.php";
+
+        if (file_exists($f)) {
+            include "$ruta/$fichero.php";
         }
-        
     }
-}
-function _listar_directorios_ruta($ruta="./"){ 
-   // abrir un directorio y listarlo recursivo 
-   if (is_dir($ruta)) { 
-      if ($dh = opendir($ruta)) { 
-         $carpetas = [];
-          while (($file = readdir($dh)) !== false) { 
-            //esta línea la utilizaríamos si queremos listar todo lo que hay en el directorio 
-            //mostraría tanto archivos como directorios 
-            //echo "<br>Nombre de archivo: $file : Es un: " . filetype($ruta . $file); 
-            if (is_dir($ruta . $file) && $file!="." && $file!=".."){ 
-               //solo si el archivo es un directorio, distinto que "." y ".." 
-               
-               $c = array_push($carpetas, $file);
-               
-                
-              //  echo "<br>Directorio: $ruta$file"; 
-               
-              // _listar_directorios_ruta($ruta . $file . "/"); 
-            } 
-         } 
-      closedir($dh); 
-      } 
-}else {
-      echo "<br>No es ruta valida"; 
-}
-return $carpetas;
-
-}
-
-
-
-function _magia_menu($selecionado){
-$menu_items = _listar_directorios_ruta();
-$menu_total_items = count(_listar_directorios_ruta());
-$i = 0;
-while ($i < $menu_total_items) {    
+  */  
     
-    $activa = ($selecionado == $menu_items[$i] )? 'active' : 0 ;  
-    if($activa){
-        $clase = ' glyphicon glyphicon-folder-open '; 
-    }else  {
-        $clase = ' glyphicon glyphicon-folder-close '; 
+}
+
+function _listar_directorios_ruta($ruta = "./") {
+    // abrir un directorio y listarlo recursivo 
+    if (is_dir($ruta)) {
+        if ($dh = opendir($ruta)) {
+            $carpetas = [];
+            while (($file = readdir($dh)) !== false) {
+                //esta línea la utilizaríamos si queremos listar todo lo que hay en el directorio 
+                //mostraría tanto archivos como directorios 
+                //echo "<br>Nombre de archivo: $file : Es un: " . filetype($ruta . $file); 
+                if (is_dir($ruta . $file) && $file != "." && $file != "..") {
+
+                    //solo si el archivo es un directorio, distinto que "." y ".." 
+
+                    $c = array_push($carpetas, $file);
+
+
+                    //  echo "<br>Directorio: $ruta$file"; 
+                    // _listar_directorios_ruta($ruta . $file . "/"); 
+                }
+            }
+            closedir($dh);
+        }
+    } else {
+        echo "<br>No es ruta valida";
+    }
+    return $c;
+}
+
+function _listar_ficheros_de_carpeta($ruta = '.') {
+    $directorio = opendir($ruta); //ruta actual
+    
+    $ficheros = [] ;
+    
+    while ($archivo = readdir($directorio)) { //obtenemos un archivo y luego otro sucesivamente
+        if (is_dir($archivo)) {//verificamos si es o no un directorio
+            //echo "[" . $archivo . "]<br />"; //de ser un directorio lo envolvemos entre corchetes            
+            $r = array_push($ficheros, $archivo);            
+        } 
     }
     
-    if($menu_items[$i] != 'home'){
-    echo '<li class="'.$activa.'"><a href="?p=' . $menu_items[$i] . '&c=index"> <span class="'.$clase.'"></span> ' . ucwords($menu_items[$i]) . '</a></li>';
-    }
-    $i++;    
+    return $r;
 }
 
+function _magia_menu($selecionado) {
+    $menu_items = _listar_directorios_ruta();
+    $menu_total_items = count(_listar_directorios_ruta());
+    $i = 0;
+    while ($i < $menu_total_items) {
+
+        $activa = ($selecionado == $menu_items[$i] ) ? 'active' : 0;
+        if ($activa) {
+            $clase = ' glyphicon glyphicon-folder-open ';
+        } else {
+            $clase = ' glyphicon glyphicon-folder-close ';
+        }
+
+        if ($menu_items[$i] != 'home') {
+            echo '<li class="' . $activa . '"><a href="?p=' . $menu_items[$i] . '&c=index"> <span class="' . $clase . '"></span> ' . ucwords($menu_items[$i]) . '</a></li>';
+        }
+        $i++;
+    }
 }
 
 function _estatus($estatus) {
-    if($estatus==0){
-        return _t("Activo","formularios");
-    }else {
-        return _t("Bloqueado","formularios");
+    if ($estatus == 0) {
+        return _t("Activo", "formularios");
+    } else {
+        return _t("Bloqueado", "formularios");
     }
 }
