0d962c82312c372586daeef52b3fcb13bb180f70
robinson <robinson@localhost>
2017-05-18 23:48:01 +0200

    add mysql_set_charset('utf8', $conexion);

--- codigo_fuente/admin/coneccion.php
+++ codigo_fuente/admin/coneccion.php
@@ -4,4 +4,4 @@
  */
 $conexion = mysql_connect("$bd_servidor", "$bd_usuario", "$bd_clave") or die("Problemas en la conexion");
 mysql_select_db("$bd_bdatos", $conexion) or die("Problemas conexion en local");
-
+mysql_set_charset('utf8', $conexion);
