d33f2789a0b95b9ff0e8669ca0c2246d6aa58f35
robincoello <robincoello@hotmail.com>
2018-03-09 16:54:27 +0100

    mep

--- gestion/_menu/controlador/index.php
+++ gestion/_menu/controlador/index.php
@@ -5,7 +5,14 @@
  $accion = "ver"; 
  $pagina = "_menu"; 
  if (permisos_tiene_permiso($accion,$pagina,$_usuarios_grupo)) { 
-  $inicia = (isset($_REQUEST['pag']) && $_REQUEST['pag'] != 0 ) ? $_REQUEST['pag'] * $cfg_limite_items_en_tablas : 0;    
+  
+    $inicia = (isset($_REQUEST['pag']) && $_REQUEST['pag'] != 0 ) ? $_REQUEST['pag'] * $cfg_limite_items_en_tablas : 0;
+    // ordenamos por id por defecto 
+    $ordenpor   = (isset($_REQUEST['ordenpor']) && $_REQUEST['ordenpor'] != '' ) ? $_REQUEST['ordenpor']  : 'id';
+    //asc / desc
+    $orden   = (isset($_REQUEST['orden']) && $_REQUEST['orden'] != '' ) ? $_REQUEST['orden']  : 'ASC';
+    
+     
      include "./_menu/modelos/index.php"; 
      include "./_menu/vista/index.php"; 
      logs_historico($p,$c,false);
--- gestion/_menu/vista/index.php
+++ gestion/_menu/vista/index.php
@@ -1,60 +1,57 @@
-<?php 
- /**  
- magia_version: 0.0.8 
- **/ ?>
-<?php // include "tabs.php"; ?>
+<?php /**
+  magia_version: 0.0.8
+ * */ ?>
+<?php // include "tabs.php";  ?>
 <h2> 
-<span class="glyphicon glyphicon-<?php echo _menu_icono_segun_pagina($p); ?>"></span> 
+    <span class="glyphicon glyphicon-<?php echo _menu_icono_segun_pagina($p); ?>"></span> 
 
-<?php echo _t("Lista de _menu"); ?> <a type="button" class="btn btn-primary navbar-btn" href="?p=_menu&c=crear"> <?php _t("Nueva"); ?></a>
+    <?php echo _t("Lista de _menu"); ?> <a type="button" class="btn btn-primary navbar-btn" href="?p=_menu&c=crear"> <?php _t("Nueva"); ?></a>
 </h2>
 
 <table class="table table-striped">
     <thead>
         <tr> 
-        <th>#</th>
-
- <th><?php echo _t("Ubicacion"); ?></th> 
- <th><?php echo _t("Padre"); ?></th> 
- <th><?php echo _t("Label"); ?></th> 
- <th><?php echo _t("Url"); ?></th> 
- <th><?php echo _t("Icono"); ?></th> 
- <th><?php echo _t("Orden"); ?></th> 
- <th><?php echo _t("Accion"); ?></th> 
- </tr>
+            <th>#</th>            
+            <th><?php ordenpor("index.php?p=$p", 'Ubicación', 'ubicacion', $orden);?></th> 
+            <th><?php ordenpor("index.php?p=$p", 'Padre', 'padre', $orden);?></th>             
+            <th><?php ordenpor("index.php?p=$p", 'Label', 'label', $orden);?></th>             
+            <th><?php ordenpor("index.php?p=$p", 'Url', 'url', $orden);?></th>             
+            <th><?php ordenpor("index.php?p=$p", 'Icono', 'icono', $orden);?></th>             
+            <th><?php ordenpor("index.php?p=$p", 'Orden', 'orden', $orden);?></th>             
+            
+            <th><?php echo _t("Accion"); ?></th> 
+        </tr>
     </thead>
     <tbody>
-    
- <?php
-   if(permisos_tiene_permiso("ver", "_menu", $_usuarios_grupo)){
-                include "./_menu/vista/tr_buscar.php";
-                
-            }
-   ?>
-   
 
         <?php
-        $i=1;
-        while ($_menu = mysql_fetch_array($sql)) {
-            include "./_menu/reg/reg.php"; 
-                if(permisos_tiene_permiso("editar", "_menu", $_usuarios_grupo)){
-                    include "./_menu/vista/tr.php";
-                   // include "./_menu/vista/tr_editar.php";
-                }else{
-                    include "./_menu/vista/tr.php";
-                }      
-            $i++;    
+        if (permisos_tiene_permiso("ver", "_menu", $_usuarios_grupo)) {
+            include "./_menu/vista/tr_buscar.php";
         }
         ?>
+
+
+<?php
+$i = 1;
+while ($_menu = mysql_fetch_array($sql)) {
+    include "./_menu/reg/reg.php";
+    if (permisos_tiene_permiso("editar", "_menu", $_usuarios_grupo)) {
+        include "./_menu/vista/tr.php";
+        // include "./_menu/vista/tr_editar.php";
+    } else {
+        include "./_menu/vista/tr.php";
+    }
+    $i++;
+}
+?>
     </tbody>
-     <?php
-   if(permisos_tiene_permiso("crear", "_menu", $_usuarios_grupo)){
-                include "./_menu/vista/tr_anadir.php";
-                
-            }
-   ?>
-    
-    
+        <?php
+        if (permisos_tiene_permiso("crear", "_menu", $_usuarios_grupo)) {
+            include "./_menu/vista/tr_anadir.php";
+        }
+        ?>
+
+
 </table> 
 
 <?php
